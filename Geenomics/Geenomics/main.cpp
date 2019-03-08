84c9d94d3dcdd3c573235d91725e101da2ffba1b
William Chern <w.chern.alt@gmail.com>
2019-03-08 00:26:59 -0800

    Add skeleton files to Xcode project

---
+++ Geenomics/Geenomics/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Geenomics
+//
+//  Created by William Chern on 3/8/19.
+//  Copyright © 2019 William Chern. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
