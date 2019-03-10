378930432cb1b9892915b905193aef2c99691675
William Chern <w.chern.alt@gmail.com>
2019-03-10 00:22:08 -0800

    Begin implementing Trie class (need to test!)

--- Geenomics/Geenomics/Trie.h
+++ Geenomics/Geenomics/Trie.h
@@ -18,6 +18,56 @@ public:
     Trie(const Trie&) = delete;
     Trie& operator=(const Trie&) = delete;
 private:
+    struct TrieNode {
+        TrieNode(const std::string& l) {
+            label = l;
+        }
+        char label;
+        std::vector<ValueType> values;
+        std::vector<TrieNode*> children;
+    };
+    TrieNode* root;
+    void resetHelper(TrieNode* n);
+    void insertHelper(const std::string& key, const ValueType& value, TrieNode* n);
 };
+
+template<typename ValueType>
+Trie<ValueType>::Trie() {
+    root = nullptr;
+}
+
+template<typename ValueType>
+Trie<ValueType>::~Trie() {
+    reset();
+}
+
+template<typename ValueType>
+void Trie<ValueType>::reset() {
+    resetHelper(root);
+}
+
+template<typename ValueType>
+void Trie<ValueType>::resetHelper(TrieNode* n) {
+    std::vector<TrieNode*>* childrenPointer = &(n->children);
+    if (childrenPointer->size() <= 0) {
+        return;
+    }
+    for (int i = 0; i < childrenPointer->size(); i++) {
+        resetHelper(n->children[i]);
+    }
+    delete n;
+}
+
+template<typename ValueType>
+void Trie<ValueType>::insert(const std::string& key, const ValueType& value) {
+    insertHelper(key, value, root);
+}
+
+template<typename ValueType>
+void Trie<ValueType>::insertHelper(const std::string& key, const ValueType& value, TrieNode* n) {
+    if (n == nullptr) {
+        
+    }
+}
 
 #endif // TRIE_INCLUDED
